11f2961a9933ef69911f7767e104fe5e76376796
amaliujia <amaliujia@163.com>
2016-05-27 22:15:50 -0400

    Add skeleton code of concurrent skip list index.

--- src/backend/index/concurrent_skip_list_index.cpp
+++ src/backend/index/concurrent_skip_list_index.cpp
@@ -11,10 +11,93 @@
 //===----------------------------------------------------------------------===//
 
 #include "concurrent_skip_list_index.h"
+#include "backend/index/btree_index.h"
+#include "backend/index/index_key.h"
+#include "backend/common/logger.h"
+#include "backend/storage/tuple.h"
 
 namespace peloton {
 namespace index {
 
+template <typename KeyType, typename ValueType>
+bool ConcurrentSkipListIndex<KeyType, ValueType>::InsertEntry(const storage::Tuple *key, const ItemPointer &location) {
+
+}
+
+template <typename KeyType, typename ValueType>
+bool ConcurrentSkipListIndex<KeyType, ValueType>::DeleteEntry(const storage::Tuple *key, const ItemPointer &location) {
+
+}
+
+template <typename KeyType, typename ValueType>
+bool ConcurrentSkipListIndex<KeyType, ValueType>::CondInsertEntry(const storage::Tuple *key, const ItemPointer &location,
+                     std::function<bool(const ItemPointer &)> predicate) {
+
+}
+
+template <typename KeyType, typename ValueType>
+void ConcurrentSkipListIndex<KeyType, ValueType>::Scan(const std::vector<Value> &values,
+          const std::vector<oid_t> &key_column_ids,
+          const std::vector<ExpressionType> &expr_types,
+          const ScanDirectionType &scan_direction,
+          std::vector<ItemPointer> &) {
+
+}
+
+template <typename KeyType, typename ValueType>
+void ConcurrentSkipListIndex<KeyType, ValueType>::ScanAllKeys(std::vector<ItemPointer> &) {
+
+}
+
+template <typename KeyType, typename ValueType>
+void ConcurrentSkipListIndex<KeyType, ValueType>::ScanKey(const storage::Tuple *key, std::vector<ItemPointer> &) {
+
+}
+
+template <typename KeyType, typename ValueType>
+void ConcurrentSkipListIndex<KeyType, ValueType>::Scan(const std::vector<Value> &values,
+          const std::vector<oid_t> &key_column_ids,
+          const std::vector<ExpressionType> &exprs,
+          const ScanDirectionType &scan_direction,
+          std::vector<ItemPointer *> &result) {
+
+}
+
+template <typename KeyType, typename ValueType>
+void ConcurrentSkipListIndex<KeyType, ValueType>::ScanAllKeys(std::vector<ItemPointer *> &result) {
+
+}
+
+template <typename KeyType, typename ValueType>
+void ConcurrentSkipListIndex<KeyType, ValueType>::ScanKey(const storage::Tuple *key, std::vector<ItemPointer *> &result) {
+
+}
+
+template <typename KeyType, typename ValueType>
+std::string ConcurrentSkipListIndex<KeyType, ValueType>::GetTypeName() const {
+  return "CONCURRENTSKIPLIST";
+}
+
+// Explicit template instantiation
+template class ConcurrentSkipListIndex<IntsKey<1>, ItemPointer *>;
+template class ConcurrentSkipListIndex<IntsKey<2>, ItemPointer *>;
+template class ConcurrentSkipListIndex<IntsKey<3>, ItemPointer *>;
+template class ConcurrentSkipListIndex<IntsKey<4>, ItemPointer *>;
+
+template class ConcurrentSkipListIndex<GenericKey<4>, ItemPointer *>;
+template class ConcurrentSkipListIndex<GenericKey<8>, ItemPointer *>;
+template class ConcurrentSkipListIndex<GenericKey<12>, ItemPointer *>;
+template class ConcurrentSkipListIndex<GenericKey<16>, ItemPointer *>;
+template class ConcurrentSkipListIndex<GenericKey<24>, ItemPointer *>;
+template class ConcurrentSkipListIndex<GenericKey<32>, ItemPointer *>;
+template class ConcurrentSkipListIndex<GenericKey<48>, ItemPointer *>;
+template class ConcurrentSkipListIndex<GenericKey<64>, ItemPointer *>;
+template class ConcurrentSkipListIndex<GenericKey<96>, ItemPointer *;
+template class ConcurrentSkipListIndex<GenericKey<128>, ItemPointer *>;
+template class ConcurrentSkipListIndex<GenericKey<256>, ItemPointer *>;
+template class ConcurrentSkipListIndex<GenericKey<512>, ItemPointer *>;
+
+template class ConcurrentSkipListIndex<TupleKey, ItemPointer *>;
 
 }
 }
--- src/backend/index/concurrent_skip_list_index.h
+++ src/backend/index/concurrent_skip_list_index.h
@@ -12,11 +12,11 @@
 
 #pragma once
 
+#include <map>
+
 #include "backend/index/index.h"
 
 #include "cds/container/skip_list_map_nogc.h"
-
-//#define CDS_DOXYGEN_INVOKED 1
 
 namespace peloton {
 namespace index {
@@ -28,13 +28,46 @@ class ConcurrentSkipListIndex : Index {
 
   typedef cds::container::SkipListMap<cds::gc::nogc, KeyType, ValueType> MapType;
 public:
-  ConcurrentSkipListIndex(IndexMetadata *metadata) :Index(metadata) { }
+  ConcurrentSkipListIndex(IndexMetadata *metadata) : Index(metadata) { }
 
   ~ConcurrentSkipListIndex() { }
 
+  bool InsertEntry(const storage::Tuple *key, const ItemPointer &location);
+
+  bool DeleteEntry(const storage::Tuple *key, const ItemPointer &location);
+
+  bool CondInsertEntry(const storage::Tuple *key, const ItemPointer &location,
+                       std::function<bool(const ItemPointer &)> predicate);
+
+  void Scan(const std::vector<Value> &values,
+            const std::vector<oid_t> &key_column_ids,
+            const std::vector<ExpressionType> &expr_types,
+            const ScanDirectionType &scan_direction,
+            std::vector<ItemPointer> &);
+
+  void ScanAllKeys(std::vector<ItemPointer> &);
+
+  void ScanKey(const storage::Tuple *key, std::vector<ItemPointer> &);
+
+  void Scan(const std::vector<Value> &values,
+            const std::vector<oid_t> &key_column_ids,
+            const std::vector<ExpressionType> &exprs,
+            const ScanDirectionType &scan_direction,
+            std::vector<ItemPointer *> &result);
+
+  void ScanAllKeys(std::vector<ItemPointer *> &result);
+
+  void ScanKey(const storage::Tuple *key, std::vector<ItemPointer *> &result);
+
+  std::string GetTypeName() const;
+
+  bool Cleanup() { return true; }
+
+  size_t GetMemoryFootprint() { return 0; }
+
 private:
 
-  MapType container_;
+  MapType container;
 };
 
 }
