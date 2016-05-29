27ea4c86143e0deb303d767b1d4fbc0f858e1977
amaliujia <amaliujia@163.com>
2016-05-29 12:56:34 -0400

    use custom traits

--- src/backend/index/concurrent_skip_list_index.cpp
+++ src/backend/index/concurrent_skip_list_index.cpp
@@ -18,29 +18,36 @@
 namespace peloton {
 namespace index {
 
-template <typename KeyType, typename ValueType>
-bool ConcurrentSkipListIndex<KeyType, ValueType>::InsertEntry(
+template <typename KeyType, typename ValueType, class KeyComparator>
+ConcurrentSkipListIndex<KeyType, ValueType, KeyComparator>::
+          ConcurrentSkipListIndex(IndexMetadata *metadata) :
+            Index(metadata) {}
+            // comparator(metadata) {}
+
+
+template <typename KeyType, typename ValueType, class KeyComparator>
+bool ConcurrentSkipListIndex<KeyType, ValueType, KeyComparator>::InsertEntry(
   UNUSED_ATTRIBUTE const storage::Tuple *key,
   UNUSED_ATTRIBUTE const ItemPointer &location) {
 
   KeyType index_key;
   index_key.SetFromKey(key);
 
-  container.insert(key, location);
+  container.insert(key, new ItemPointer(location));
 
   return true;
 }
 
-template <typename KeyType, typename ValueType>
-bool ConcurrentSkipListIndex<KeyType, ValueType>::DeleteEntry(
+template <typename KeyType, typename ValueType, class KeyComparator>
+bool ConcurrentSkipListIndex<KeyType, ValueType, KeyComparator>::DeleteEntry(
   UNUSED_ATTRIBUTE const storage::Tuple *key,
   UNUSED_ATTRIBUTE const ItemPointer &location) {
 
   return false;
 }
 
-template <typename KeyType, typename ValueType>
-bool ConcurrentSkipListIndex<KeyType, ValueType>::CondInsertEntry(
+template <typename KeyType, typename ValueType, class KeyComparator>
+bool ConcurrentSkipListIndex<KeyType, ValueType, KeyComparator>::CondInsertEntry(
   UNUSED_ATTRIBUTE const storage::Tuple *key,
   UNUSED_ATTRIBUTE const ItemPointer &location,
   UNUSED_ATTRIBUTE std::function<bool(const ItemPointer &)> predicate) {
@@ -48,63 +55,64 @@ bool ConcurrentSkipListIndex<KeyType, ValueType>::CondInsertEntry(
   return false;
 }
 
-template <typename KeyType, typename ValueType>
-void ConcurrentSkipListIndex<KeyType, ValueType>::Scan(
+template <typename KeyType, typename ValueType, class KeyComparator>
+void ConcurrentSkipListIndex<KeyType, ValueType, KeyComparator>::Scan(
   UNUSED_ATTRIBUTE const std::vector<Value> &values,
   UNUSED_ATTRIBUTE const std::vector<oid_t> &key_column_ids,
   UNUSED_ATTRIBUTE const std::vector<ExpressionType> &expr_types,
   UNUSED_ATTRIBUTE const ScanDirectionType &scan_direction, std::vector<ItemPointer> &) {}
 
-template <typename KeyType, typename ValueType>
-void ConcurrentSkipListIndex<KeyType, ValueType>::ScanAllKeys(
+template <typename KeyType, typename ValueType, class KeyComparator>
+void ConcurrentSkipListIndex<KeyType, ValueType, KeyComparator>::ScanAllKeys(
   UNUSED_ATTRIBUTE std::vector<ItemPointer> &) {}
 
-template <typename KeyType, typename ValueType>
-void ConcurrentSkipListIndex<KeyType, ValueType>::ScanKey(
+template <typename KeyType, typename ValueType, class KeyComparator>
+void ConcurrentSkipListIndex<KeyType, ValueType, KeyComparator>::ScanKey(
   UNUSED_ATTRIBUTE const storage::Tuple *key,
   UNUSED_ATTRIBUTE std::vector<ItemPointer> &) {}
 
-template <typename KeyType, typename ValueType>
-void ConcurrentSkipListIndex<KeyType, ValueType>::Scan(
+template <typename KeyType, typename ValueType, class KeyComparator>
+void ConcurrentSkipListIndex<KeyType, ValueType, KeyComparator>::Scan(
   UNUSED_ATTRIBUTE const std::vector<Value> &values,
   UNUSED_ATTRIBUTE const std::vector<oid_t> &key_column_ids,
   UNUSED_ATTRIBUTE const std::vector<ExpressionType> &exprs,
   UNUSED_ATTRIBUTE const ScanDirectionType &scan_direction,
   UNUSED_ATTRIBUTE std::vector<ItemPointer *> &result) {}
 
-template <typename KeyType, typename ValueType>
-void ConcurrentSkipListIndex<KeyType, ValueType>::ScanAllKeys(
+template <typename KeyType, typename ValueType, class KeyComparator>
+void ConcurrentSkipListIndex<KeyType, ValueType, KeyComparator>::ScanAllKeys(
   UNUSED_ATTRIBUTE std::vector<ItemPointer *> &result) {}
 
-template <typename KeyType, typename ValueType>
-void ConcurrentSkipListIndex<KeyType, ValueType>::ScanKey(
+template <typename KeyType, typename ValueType, class KeyComparator>
+void ConcurrentSkipListIndex<KeyType, ValueType, KeyComparator>::ScanKey(
   UNUSED_ATTRIBUTE const storage::Tuple *key,
   UNUSED_ATTRIBUTE std::vector<ItemPointer *> &result) {}
 
-template <typename KeyType, typename ValueType>
-std::string ConcurrentSkipListIndex<KeyType, ValueType>::GetTypeName() const {
+template <typename KeyType, typename ValueType, class KeyComparator>
+std::string ConcurrentSkipListIndex<KeyType, ValueType, KeyComparator>::GetTypeName() const {
   return "CONCURRENTSKIPLIST";
 }
 
 // Explicit template instantiation
-template class ConcurrentSkipListIndex<IntsKey<1>, ItemPointer *>;
-template class ConcurrentSkipListIndex<IntsKey<2>, ItemPointer *>;
-template class ConcurrentSkipListIndex<IntsKey<3>, ItemPointer *>;
-template class ConcurrentSkipListIndex<IntsKey<4>, ItemPointer *>;
+template class ConcurrentSkipListIndex<IntsKey<1>, ItemPointer *, IntsComparator<1>>;
+template class ConcurrentSkipListIndex<IntsKey<2>, ItemPointer *, IntsComparator<2>>;
+template class ConcurrentSkipListIndex<IntsKey<3>, ItemPointer *, IntsComparator<3>>;
+template class ConcurrentSkipListIndex<IntsKey<4>, ItemPointer *, IntsComparator<4>>;
 
-template class ConcurrentSkipListIndex<GenericKey<4>, ItemPointer *>;
-template class ConcurrentSkipListIndex<GenericKey<8>, ItemPointer *>;
-template class ConcurrentSkipListIndex<GenericKey<12>, ItemPointer *>;
-template class ConcurrentSkipListIndex<GenericKey<16>, ItemPointer *>;
-template class ConcurrentSkipListIndex<GenericKey<24>, ItemPointer *>;
-template class ConcurrentSkipListIndex<GenericKey<32>, ItemPointer *>;
-template class ConcurrentSkipListIndex<GenericKey<48>, ItemPointer *>;
-template class ConcurrentSkipListIndex<GenericKey<64>, ItemPointer *>;
-template class ConcurrentSkipListIndex < GenericKey<96>, ItemPointer *>;
-template class ConcurrentSkipListIndex<GenericKey<128>, ItemPointer *>;
-template class ConcurrentSkipListIndex<GenericKey<256>, ItemPointer *>;
-template class ConcurrentSkipListIndex<GenericKey<512>, ItemPointer *>;
+template class ConcurrentSkipListIndex<GenericKey<4>, ItemPointer *, IntsComparator<4>>;
+template class ConcurrentSkipListIndex<GenericKey<8>, ItemPointer *, IntsComparator<8>>;
+template class ConcurrentSkipListIndex<GenericKey<12>, ItemPointer *, IntsComparator<12>>;
+template class ConcurrentSkipListIndex<GenericKey<16>, ItemPointer *, IntsComparator<16>>;
+template class ConcurrentSkipListIndex<GenericKey<24>, ItemPointer *, IntsComparator<24>>;
+template class ConcurrentSkipListIndex<GenericKey<32>, ItemPointer *, IntsComparator<32>>;
+template class ConcurrentSkipListIndex<GenericKey<48>, ItemPointer *, IntsComparator<48>>;
+template class ConcurrentSkipListIndex<GenericKey<64>, ItemPointer *, IntsComparator<64>>;
+template class ConcurrentSkipListIndex < GenericKey<96>, ItemPointer *, IntsComparator<96>>;
+template class ConcurrentSkipListIndex<GenericKey<128>, ItemPointer *, IntsComparator<128>>;
+template class ConcurrentSkipListIndex<GenericKey<256>, ItemPointer *, IntsComparator<246>>;
+template class ConcurrentSkipListIndex<GenericKey<512>, ItemPointer *, IntsComparator<512>>;
 
-template class ConcurrentSkipListIndex<TupleKey, ItemPointer *>;
-}
-}
+template class ConcurrentSkipListIndex<TupleKey, ItemPointer *, TupleKeyComparator>;
+
+}  // namespace index
+}  // namespace peloton
--- src/backend/index/concurrent_skip_list_index.h
+++ src/backend/index/concurrent_skip_list_index.h
@@ -13,21 +13,64 @@
 #pragma once
 
 #include <map>
+
 #include "backend/index/index.h"
+#include "backend/index/index_key.h"
 #include "cds/container/skip_list_map_nogc.h"
+
 
 namespace peloton {
 namespace index {
 
-template <typename KeyType, typename ValueType>
+
+//template <std::size_t KeySize> struct skip_list_key_comparator {
+//  skip_list_key_comparator(index::IndexMetadata *metadata)
+//  : schema(metadata->GetKeySchema()) {}
+//
+//  inline bool operator()(const GenericKey<KeySize> &lhs,
+//                         const GenericKey<KeySize> &rhs) const {
+//    /*
+//    storage::Tuple lhTuple(schema);
+//    lhTuple.MoveToTuple(reinterpret_cast<const void *>(&lhs));
+//    storage::Tuple rhTuple(schema);
+//    rhTuple.MoveToTuple(reinterpret_cast<const void *>(&rhs));
+//    // lexicographical compare could be faster for fixed N
+//    int diff = lhTuple.Compare(rhTuple);
+//    return diff < 0;
+//    */
+//
+//    for (oid_t column_itr = 0; column_itr < schema->GetColumnCount();
+//         column_itr++) {
+//      const Value lhs_value = lhs.ToValueFast(schema, column_itr);
+//      const Value rhs_value = rhs.ToValueFast(schema, column_itr);
+//
+//      int diff = lhs_value.Compare(rhs_value);
+//
+//      if (diff) {
+//        return diff < 0;
+//      }
+//    }
+//
+//
+//
+//  const catalog::Schema *schema;
+//};
+
+
+template <typename KeyType, typename ValueType, class KeyComparator>
 class ConcurrentSkipListIndex : Index {
   friend class IndexFactory;
 
-  typedef cds::container::SkipListMap<cds::gc::nogc, KeyType, ValueType>
-      MapType;
+  struct peloton_skip_list_traits : public cds::container::skip_list::traits {
+    typedef KeyComparator compare;
+  };
+
+  typedef cds::container::SkipListMap<cds::gc::nogc, KeyType, ValueType, peloton_skip_list_traits> MapType;
+
+
 
  public:
-  ConcurrentSkipListIndex(IndexMetadata *metadata) : Index(metadata) {}
+  ConcurrentSkipListIndex(IndexMetadata *metadata);
 
   ~ConcurrentSkipListIndex() {}
 
@@ -65,6 +108,9 @@ class ConcurrentSkipListIndex : Index {
   size_t GetMemoryFootprint() { return 0; }
 
  private:
+  // KeyComparator
+  // KeyComparator comparator;
+
   MapType container;
 };
 }
